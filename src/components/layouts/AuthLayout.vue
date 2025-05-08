ed3740d221c18d04901db159e34550fca3342671
r3ndo0 <r3ndo0@gmail.com>
2025-05-08 22:00:30 +0530

    feat:tailwind and auth layout implementation

---
+++ src/components/layouts/AuthLayout.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="flex h-screen bg-neutral-bg2">
+    <div class="m-auto"><slot /></div>
+  </main>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AuthLayout from '@/components/layouts/AuthLayout.vue'
+</script>
+
+<template>
+  <AuthLayout> Register </AuthLayout>
+</template>
